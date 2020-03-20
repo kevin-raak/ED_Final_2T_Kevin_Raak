5f36eafc8bae09d6349893f31dd61bd9abdca2c0
Kraze <Kraze@DESKTOP-HPO3RBK>
2020-03-20 21:54:44 +0100

    estructuras de control

---
+++ EJCondicionesCompuestas/ED_Final_T2_Kevin_Raak/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+/*******************************
+ * Autor: Kevin Rääk
+ * Curso: 1-DAM-M
+ * Asignación: Ejercicio de condiciones compuestas
+ * Fecha de asignación: 5 de febrero
+ * *****************************/
+
+namespace EjCondicionesCompuestas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mes;
+            Console.WriteLine("Escriba un mes: ");
+            mes = Console.ReadLine();
+
+            if (mes.ToLower().Equals("enero") || mes.ToLower().Equals("febrero") || mes.ToLower().Equals("marzo") || mes.ToLower().Equals("ene") || mes.ToLower().Equals("feb") || mes.ToLower().Equals("mar"))
+            {
+                Console.WriteLine(mes + " SI pertenece al primer trimestre del año.");
+            }
+            else
+            {
+                Console.WriteLine(mes + " NO pertenece al primer trimestre del año.");
+            }
+        }
+    }
+}
---
+++ Ejercicio con estructuras de control/Ejercicio con estructuras de control/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Ejercicio_con_estructuras_de_control
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random ran = new Random();
+
+            Console.WriteLine("Escribe cuántos números quieres generar:");
+            int numero = int.Parse(Console.ReadLine());
+
+            String numeros = "";
+            String numerosPares = "";
+            int contadorPares = 0;
+            int[] pares = new int[numero];
+            for (int i = 0; i < numero; i++)
+            {
+                int num = ran.Next(0, 100);
+                numeros += num + " ";
+                if ((num % 2) == 0)
+                {
+                    numerosPares += num + " ";
+                    pares[contadorPares] = num;
+                    contadorPares++;
+                }
+            }
+
+            Console.WriteLine("Los números generados son:\n" + numeros);
+            if (contadorPares > 0)
+            {
+                Console.WriteLine("De los cuáles, éstos son pares:\n" + numerosPares);
+
+                if (contadorPares > 1)
+                {
+                    String productoPares = "";
+                    int producto = 1;
+                    for (int i = 0; i <= contadorPares - 1; i++)
+                    {
+                        if (i != contadorPares - 1)
+                        {
+                            productoPares += pares[i] + "x";
+                        }
+                        else
+                        {
+                            productoPares += pares[i];
+                        }
+                        producto = producto * pares[i];
+                    }
+                    Console.WriteLine("El producto de " + productoPares + " es: " + producto);
+                }
+            }
+            else
+            {
+                Console.WriteLine("De los cuáles, ninguno es par.");
+            }
+
+        }
+    }
+
+}
